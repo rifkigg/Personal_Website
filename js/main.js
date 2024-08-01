a6c917b048bdd05e2dbc40424fd40860ccc780d9
rifkigg <rifkiadi348@gmail.com>
2024-08-01 11:47:59 +0700

    menambah fitur kecil di navbar my project

--- js/main.js
+++ js/main.js
@@ -45,3 +45,14 @@ function handleIframeLoad() {
   window.location.reload();
   document.getElementById("contactForm").reset();
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const tabs = document.querySelectorAll(".nav-link");
+  tabs.forEach((tab) => {
+    tab.addEventListener("click", function (event) {
+      setTimeout(() => {
+        event.target.scrollIntoView({ behavior: "smooth", inline: "start" });
+      }, 100); // Delay to ensure the tab is activated
+    });
+  });
+});
